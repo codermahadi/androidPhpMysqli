d205724fbe8dc3b2fea1524a10328ef0f54c7825
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-03-28 14:12:11 +0600

    Register form desing only

---
+++ app/src/main/java/com/example/mahadi/androidphpmysql/Constants.java
@@ -0,0 +1,11 @@
+package com.example.mahadi.androidphpmysql;
+
+/**
+ * Created by Mahadi on 3/28/2018.
+ */
+
+public class Constants {
+
+    public static final String ROOT_URL="http://192.168.6.235/androidApi/v1/";
+    public static final String ROOT_REGISTER=ROOT_URL+"registerUser.php";
+}
